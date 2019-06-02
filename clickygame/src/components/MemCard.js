30c8e748bcb4af5b7a2c5919c610ffebe4d08682
Andy Pack <thepackpater@gmail.com>
2019-06-01 20:55:54 -0500

    building basics

---
+++ clickygame/src/components/MemCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function MemCard(){
+    return(
+        <div className="card">
+            <p>Stuff on a card</p>
+        </div>
+    )
+};
+
+export default MemCard;
+

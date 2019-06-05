7eef3486aa7da902c13fcda652a450b7f5a53093
Andy Pack <thepackpater@gmail.com>
2019-06-05 12:50:29 -0500

    update heroku not working

---
+++ clickygame/build/precache-manifest.03bb9eb48970a24e04e2fac94a317277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1a1d94547cb08b4b4b441329215f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c8cf8fe06c93031747c",
+    "url": "/static/css/main.0a5f7603.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "2c8cf8fe06c93031747c",
+    "url": "/static/js/main.649c97a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb193f0497c676e93daf77da9d51c8e4",
+    "url": "/static/media/AcardBack.bb193f04.jpg"
+  }
+]);
